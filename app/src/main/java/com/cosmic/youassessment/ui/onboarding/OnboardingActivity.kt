8f2e594599e7d5e6a67814d46c796bb86c8c87ab
Emmanuel Afolabi <emmanuel.a.oluwatobi@gmail.com>
2025-04-11 08:36:06 +0100

    improve onboarding view with set fonts and update images

--- app/src/main/java/com/cosmic/youassessment/ui/onboarding/OnboardingActivity.kt
+++ app/src/main/java/com/cosmic/youassessment/ui/onboarding/OnboardingActivity.kt
@@ -5,11 +5,26 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.pager.HorizontalPager
 import androidx.compose.foundation.pager.rememberPagerState
-import androidx.compose.material3.*
-import androidx.compose.runtime.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -18,10 +33,8 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import android.content.Intent
-import androidx.core.content.ContextCompat.startActivity
-import com.cosmic.youassessment.MainActivity
 import com.cosmic.youassessment.R
+import com.cosmic.youassessment.ui.theme.CapriolaFamily
 
 class OnboardingActivity : ComponentActivity() {
     @OptIn(ExperimentalFoundationApi::class)
@@ -40,23 +53,42 @@ fun OnboardingScreen() {
         OnboardingPage(
             title = "Track Your Expenses",
             description = "Get insights into where your money goes and make informed financial decisions.",
-            imageRes = R.drawable.ic_onboarding_expenses
+            imageRes = R.drawable.trading_app
         ),
         OnboardingPage(
             title = "Set Savings Goals",
             description = "Whether it's for a vacation, a new car, or an emergency fund, we help you stay on track.",
-            imageRes = R.drawable.ic_onboarding_savings
+            imageRes = R.drawable.growing_money
         ),
         OnboardingPage(
             title = "Get Financial Insights",
             description = "Access detailed reports and analytics to make better financial choices.",
-            imageRes = R.drawable.ic_onboarding_insights
+            imageRes = R.drawable.youassessimg_one
         )
     )
 
     val pagerState = rememberPagerState(pageCount = { pages.size })
 
     Column(modifier = Modifier.fillMaxSize()) {
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 16.dp, vertical = 8.dp),
+            horizontalArrangement = Arrangement.spacedBy(8.dp)
+        ) {
+            pages.forEachIndexed { index, _ ->
+                Box(
+                    modifier = Modifier
+                        .weight(7f)
+                        .height(6.dp)
+                        .background(
+                            color = if (index <= pagerState.currentPage) Color(0xFF000000) else Color(0xFFE0E0E0),
+                            shape = RoundedCornerShape(8.dp)
+                        )
+                )
+            }
+        }
+        
         HorizontalPager(
             state = pagerState,
             modifier = Modifier
@@ -84,7 +116,10 @@ fun OnboardingScreen() {
                     containerColor = Color(0xFF008080) // Teal color
                 )
             ) {
-                Text("Create an account")
+                Text(
+                    "Create an account",
+                    fontFamily = CapriolaFamily,
+                )
             }
 
             TextButton(
@@ -96,6 +131,7 @@ fun OnboardingScreen() {
             ) {
                 Text(
                     text = "Already have an account? Sign in",
+                    fontFamily = CapriolaFamily,
                     color = Color(0xFF008080) // Teal color
                 )
             }
@@ -112,25 +148,35 @@ fun OnboardingPage(page: OnboardingPage) {
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Center
     ) {
+        Column (
+            horizontalAlignment = Alignment.Start
+        ) {
+            Text(
+                text = page.title,
+                style = MaterialTheme.typography.displayLarge.copy(
+                    fontSize = 42.sp,
+                    lineHeight = 40.sp,
+                    fontFamily = CapriolaFamily,
+                    fontWeight = FontWeight.Bold
+                ),
+                textAlign = TextAlign.Start
+            )
+            Spacer(modifier = Modifier.height(16.dp))
+            Text(
+                text = page.description,
+                style = MaterialTheme.typography.bodyLarge.copy(
+                    fontFamily = CapriolaFamily,
+                    fontWeight = FontWeight.Bold
+                ),
+                textAlign = TextAlign.Start
+            )
+        }
         Image(
             painter = painterResource(id = page.imageRes),
             contentDescription = null,
-            modifier = Modifier.size(200.dp)
-        )
-        Spacer(modifier = Modifier.height(32.dp))
-        Text(
-            text = page.title,
-            fontSize = 28.sp,
-            fontWeight = FontWeight.Bold,
-            textAlign = TextAlign.Center
+            modifier = Modifier.size(400.dp)
         )
         Spacer(modifier = Modifier.height(16.dp))
-        Text(
-            text = page.description,
-            fontSize = 16.sp,
-            textAlign = TextAlign.Center,
-            color = Color.Gray
-        )
     }
 }
 
--- app/src/main/java/com/cosmic/youassessment/ui/theme/Type.kt
+++ app/src/main/java/com/cosmic/youassessment/ui/theme/Type.kt
@@ -2,33 +2,45 @@ package com.cosmic.youassessment.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.cosmic.youassessment.R
 
-// Set of Material typography styles to start with
+// Create a FontFamily from the Capriola font
+val CapriolaFamily = FontFamily(
+    Font(R.font.capriola_regular)
+)
+
+// Set of Material typography styles using Capriola font
 val Typography = Typography(
+    displayLarge = TextStyle(
+        fontFamily = CapriolaFamily,
+        fontWeight = FontWeight.Normal,
+        fontSize = 32.sp,
+        lineHeight = 40.sp,
+        letterSpacing = (-0.02).sp
+    ),
     bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
+        fontFamily = CapriolaFamily,
         fontWeight = FontWeight.Normal,
         fontSize = 16.sp,
         lineHeight = 24.sp,
         letterSpacing = 0.5.sp
-    )
-    /* Other default text styles to override
+    ),
     titleLarge = TextStyle(
-        fontFamily = FontFamily.Default,
+        fontFamily = CapriolaFamily,
         fontWeight = FontWeight.Normal,
         fontSize = 22.sp,
         lineHeight = 28.sp,
         letterSpacing = 0.sp
     ),
     labelSmall = TextStyle(
-        fontFamily = FontFamily.Default,
+        fontFamily = CapriolaFamily,
         fontWeight = FontWeight.Medium,
         fontSize = 11.sp,
         lineHeight = 16.sp,
         letterSpacing = 0.5.sp
     )
-    */
 )
